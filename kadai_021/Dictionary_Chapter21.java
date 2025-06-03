1118e2fe6029a5ad261d18724a45e836cf74c00f
nagi-vc <vivi.coha.3388@gmail.com>
2025-06-03 22:32:26 +0900

    kadai

---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,35 @@
+package kadai_021;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+	private HashMap<String, String> fruitMap;
+
+	public Dictionary_Chapter21() {
+	fruitMap = new HashMap<>();
+	tuika();
+	
+	}
+	
+	public void tuika() {
+		fruitMap.put("apple","りんご");
+		fruitMap.put("peach","桃");
+		fruitMap.put("banana","バナナ");
+		fruitMap.put("lemon","レモン");
+		fruitMap.put("pear","梨");
+		fruitMap.put("kiwi","キウイ");
+		fruitMap.put("strawberry","いちご");
+		fruitMap.put("grape","ぶどう");
+		fruitMap.put("muscat","マスカット");
+		fruitMap.put("cherry","さくらんぼ");
+	}
+	public void searchWords(String[] words) {
+		for(String word : words) {
+			if(fruitMap.containsKey(words)) {
+				System.out.println(word + "の意味は" +fruitMap.get(words) + "です");
+			}else {
+				System.out.println(word + "は辞書にありません");
+			}
+		}
+	}
+}

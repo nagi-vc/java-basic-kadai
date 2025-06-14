407e03976b39fc113407052f26e5d34339db79d0
nagi-vc <vivi.coha.3388@gmail.com>
2025-06-14 13:42:53 +0900

    kadai

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -3,16 +3,16 @@ package kadai_018;
 public class KatoExec_Chapter18 {
 	public static void main(String[] args) {
 
-		Kato_Chapter18 taro = new KatoTaro_Chapter18();
-		taro.setGivenName("太郎");
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18(); 
+		taro.setGivenName();
 		taro.execIntroduce();
 
-		Kato_Chapter18 ichiro = new KatoIchiro_Chapter18();
-		ichiro.setGivenName("一郎");
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		ichiro.setGivenName();
 		ichiro.execIntroduce();
 
-		Kato_Chapter18 hanako = new KatoHanako_Chapter18();
-		hanako.setGivenName("花子");
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		hanako.setGivenName();
 		hanako.execIntroduce();
 	}
 }
--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -6,10 +6,6 @@ public abstract class Kato_Chapter18 {
 	public String givenName;
 	public String address = "東京都中野区◯×";
 	
-	public void setGivenName(String givenName) {
-		this.givenName = givenName;
-	}
-	
 	
 	public void commonIntroduce() {
 		System.out.println("名前は " + familyName + givenName + "です" );
